8aeb3dd7979c28b1c9d363ff6c32cdfb532bbba9
rome <99492438+wildgen3@users.noreply.github.com>
2022-06-06 22:08:33 -0500

    Updated flake.nix with host specific configuration

--- flake.nix
+++ flake.nix
@@ -33,6 +33,20 @@
 				./modules/ssh-phone-home.nix
 				./users/users.nix
 	};
+deploy = {
+	
+		nodes.lab1 = {
 
-  };
+			hostname = "192.168.1.156";
+			profiles.system = {
+				user = "root":
+				path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.lab1;
+				};
+
+		};
+	};
+	
+};
+
 }
+
